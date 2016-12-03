f69a6d7f4c5b3edbe8d726a9e44a87baac4ed5ad
oozzy <oozzy.tlo@gmail.com>
2016-12-03 07:54:14 -0800

    Remove magic number

--- socket_test.c
+++ socket_test.c
@@ -4,24 +4,25 @@
 #include <tlo/socket.h>
 #include "tlocposix_test.h"
 
-#define SOME_PORT "12345"
+#define SOME_PORT_STR "12345"
+#define SOME_PORT_INT 12345
 
 static void testGetBindableWildcardAddress(void) {
-  struct addrinfo *addresses = tloGetBindableWildcardAddress(SOME_PORT);
+  struct addrinfo *addresses = tloGetBindableWildcardAddress(SOME_PORT_STR);
   assert(addresses);
 
   for (struct addrinfo *address = addresses; address != NULL;
        address = address->ai_next) {
     printf("%s|%u\n", tloGetAddress(address->ai_addr),
            tloGetPort(address->ai_addr));
-    assert(tloGetPort(address->ai_addr) == 12345);
+    assert(tloGetPort(address->ai_addr) == SOME_PORT_INT);
   }
 
   freeaddrinfo(addresses);
 }
 
 static void testGetSocketBoundToReusableAddress(void) {
-  struct addrinfo *addresses = tloGetBindableWildcardAddress(SOME_PORT);
+  struct addrinfo *addresses = tloGetBindableWildcardAddress(SOME_PORT_STR);
   int socketfd = tloGetSocketBoundToReusableAddress(addresses);
   assert(socketfd != TLO_SOCKET_ERROR);
   freeaddrinfo(addresses);
